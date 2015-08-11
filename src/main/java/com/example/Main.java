df628b5c853a6f0de95b32a955836d22a07ef3a9
shan_xunbao <guangshanw@163.com>
2015-08-11 11:56:56 +0800

    this is test master switch

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -36,6 +36,7 @@ public class Main {
      */
     public static void main(String[] args) throws IOException {
     	System.out.println("this is test git the second commit");
+    	System.out.println("this is test master switch");
         final HttpServer server = startServer();
         System.out.println(String.format("Jersey app started with WADL available at "
                 + "%sapplication.wadl\nHit enter to stop it...", BASE_URI));
